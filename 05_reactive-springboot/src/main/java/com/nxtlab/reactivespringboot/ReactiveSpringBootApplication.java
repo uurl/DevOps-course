1de17133caf82cfad8ec477bb53b64a32744a9c8
nxtlab@nxtlab.mx <nxtlab@nxtlab.mx>
2019-12-02 15:01:54 -0600

    Reactive Spring

--- 05_reactive-springboot/src/main/java/com/nxtlab/reactivespringboot/ReactiveSpringBootApplication.java
+++ 05_reactive-springboot/src/main/java/com/nxtlab/reactivespringboot/ReactiveSpringBootApplication.java
@@ -4,9 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootReactiveApplication {
-
+public class ReactiveSpringBootApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootReactiveApplication.class, args);
+		SpringApplication.run(ReactiveSpringBootApplication.class, args);
 	}
 }
--- 05_reactive-springboot/src/main/java/com/nxtlab/reactivespringboot/service/CoinService.java
+++ 05_reactive-springboot/src/main/java/com/nxtlab/reactivespringboot/service/CoinService.java
@@ -1,7 +1,7 @@
 package com.nxtlab.reactivespringboot.service;
 
 import com.nxtlab.reactivespringboot.model.Coins;
-import com.nxtlab.reactivespringboot.SingleTicker;
+import com.nxtlab.reactivespringboot.model.SingleTicker;
 import com.nxtlab.reactivespringboot.model.Tickers;
 
 import reactor.core.publisher.Mono;
